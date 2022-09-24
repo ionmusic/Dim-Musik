c5667cd95a7a5b3c3e62b37e0f6d382eb64af94f
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-24 08:17:37 +0700

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -6,7 +6,7 @@ HELP_TEXT = """
 ʜᴇʟʟᴏ [{}](tg://user?id={})
 ɪ ᴀᴍ sᴜᴘᴇʀ ғᴀsᴛ ɴᴇxᴛ ɢᴇɴᴇʀᴀᴛɪᴏɴ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ᴀɴᴅ ɴᴏ ʟᴀɢ ɪssᴜᴇ ᴡɪᴛʜ ʙᴇsᴛ sᴏᴜɴᴅ ǫᴜᴀʟɪᴛʏ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs
 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ
-ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ [sᴜᴍɪᴛ ʏᴀᴅᴀᴠ](https://t.me/Mr_DiSasTer_XD)...
+ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ [𝐊𝐋𝐘 ༱ 𝐇𝐀𝐍𝐃𝐁𝐄𝐀 🇦🇱](https://t.me/Klyuserbot)...
 ━━━━━━━━━━━━━━━━━━━**"""
 
 
@@ -16,14 +16,14 @@ async def home(_, query: CallbackQuery):
     reply_markup=InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            InlineKeyboardButton("➕ Tambahkan Saya Ke Group Anda ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-            InlineKeyboardButton("🍄 sᴜᴘᴘᴏʀᴛ", url="https://t.me/TechQuardSupport"),
+            InlineKeyboardButton("🫂 sᴜᴘᴘᴏʀᴛ", url="https://t.me/TechQuardSupport"),
             InlineKeyboardButton("📣 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/TechQuard")
         ],
         [
-            InlineKeyboardButton("🧰 ᴄᴏᴍᴍᴀɴᴅs", url="https://telegra.ph/%F0%9D%99%B2%E1%B4%8F%E1%B4%8D%E1%B4%8D%E1%B4%80%C9%B4%E1%B4%85s-04-06"),
+            InlineKeyboardButton("🧰 ᴄᴏᴍᴍᴀɴᴅs", url="https://telegra.ph/Dim-COMMAND-21-11-04"),
             InlineKeyboardButton("⚕️ ᴍᴏʀᴇ ɪɴғᴏ", callback_data="moreinfo")
         ]
    
@@ -41,18 +41,18 @@ async def others(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""ʜᴇʏᴀ [{query.message.chat.first_name}](tg://user?id={query.message.chat.id})
 
-ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ᴍᴇ :""",
+Dim Musik untuk Telegram :""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🗯️ ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/TechQuardSupport"),
+                        "🗯️ ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/suportsipuserbot"),
                     InlineKeyboardButton(
-                        "🌐 ᴜᴘᴅᴀᴛᴇꜱ", url=f"https://t.me/TechQuard")
+                        "🌐 ᴜᴘᴅᴀᴛᴇꜱ", url=f"https://t.me/suportNande")
                 ],
                 [
                     InlineKeyboardButton(
-                        "🍄 ᴍᴀɪɴᴛᴀɪɴᴇʀ", url="https://t.me/Mr_Disaster_Xd"),
+                        "🍄 ᴍᴀɪɴᴛᴀɪɴᴇʀ", url="https://t.me/Klyuserbot"),
                     InlineKeyboardButton(
                         "🍀 ᴏᴛʜᴇʀ ɪɴғᴏ", callback_data="repoinfo")
                 ],
@@ -77,24 +77,25 @@ async def reinfo(_, query: CallbackQuery):
 @Client.on_callback_query(filters.regex("repoinfo"))
 async def repoinfo(_, query: CallbackQuery):
     await query.edit_message_text(
-        f"""ʜᴇʀᴇ ᴀʙᴏᴜᴛ ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : 
-ᴀɴᴅ ʙᴏᴛ ʟɪsᴛs ᴀɴᴅ ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴏᴜʀ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ ᴏʀ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ.
-ᴛʜɪs ʀᴇᴘᴏ ɪs ᴏɴʟʏ ᴍᴀᴅᴇ ғᴏʀ ᴅᴇᴘʟᴏʏɪɴɢ ᴀ ᴘᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ʙᴏᴛ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ғᴀᴄɪɴɢ ʜᴇʀᴏᴋᴜ ᴀᴄᴄᴏᴜɴᴛ ʙᴀɴɴɪɴɢ ᴘʀᴏʙᴇʟᴍ.
+        f"""Selengkapnya Tentang Saya : 
+Tidak banyak fitur lainnya dan tolong bergabung dengan saluran atau saluran Instagram.
+Repo ini hanya dibuat untuk menyebarkan bot musik yang kuat di heroku tanpa menghadapi masalah pemblokiran akun heroku.
+.
 
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔗 ɢɪᴛʜᴜʙ", url=f"https://github.com/Sumit9969/DarkxMusic"),
+                        "🔗 ɢɪᴛʜᴜʙ", url=f"https://github.com/sip-Userbot/Dim-Musik"),
                     InlineKeyboardButton(
-                        "💌 ʏᴏᴜᴛᴜʙᴇ", url=f"https://youtube.com/channel/UCtI7hbY-BD7wvuIzoSU0cEw")
+                        "💌 Instagram", url=f"https://instagram.com/kanjeng_47?igshid=YmMyMTA2M2Y=")
                 ],
                 [
                     InlineKeyboardButton(
-                        "👾 ʙᴏᴛ ʟɪsᴛs", url="https://t.me/TechQuardBot"),
+                        "👾 ʙᴏᴛ ʟɪsᴛs", url="https://t.me/suportsipuserbot"),
                     InlineKeyboardButton(
-                        "🤤 ᴘᴏʀɴ ʜᴜʙ", url="http://t.me/TheNudesHubBot")
+                        "🤤 ᴘᴏʀɴ ʜᴜʙ", url="http://t.me/suportNande")
                 ],
                 [
                     InlineKeyboardButton("⟲ ʙᴀᴄᴋ ⟳", callback_data="moreinfo")
